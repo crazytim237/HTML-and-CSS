43837c2d8cac6110637f406d662ffa82feb1f83f
crazytim237 <87796455+crazytim237@users.noreply.github.com>
2021-10-14 05:39:11 -0400

    CSS practice
    i learn to us ol ul and shadowing and also how to use color with CSS

--- Basic_HTML_and_javascript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_javascript_1/Basic_JavaScript_1.js
@@ -1,1 +1,9 @@
-alert("Hello, World");
+<script>
+        function My_First_Function() {
+            var string = "kiss me, I'm Irish!";
+            var result = string.fontcolor("green");
+            document.getElementById("Irish").innerHTML = result;
+        
+    </script>
+
+
